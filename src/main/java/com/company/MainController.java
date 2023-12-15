ce505ddfb8cb4b25a7465800eee26520e91ce72b
Matias Antiñanco <matias.antinanco@gmail.com>
2023-12-15 10:04:35 -0300

    Adding logging to app. So many options for sth so simple?

--- src/main/java/com/company/MainController.java
+++ src/main/java/com/company/MainController.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,13 @@ import com.company.utils.SupportedCommands;
 @RestController
 public class MainController {
 
+ 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
 	@GetMapping("/install")
     public String install(@RequestParam(value = "package") String packageName) {
       
+      logger.debug("Package command received...");
+
       BrokerCommand brokerCommand = new BrokerCommand();
       String inputCommand = SupportedCommands.INSTALL_COMMAND + " " + packageName;
       String result = brokerCommand.executeCommand(inputCommand);
@@ -25,6 +31,8 @@ public class MainController {
   @GetMapping("/list")
     public String list() {
 
+      logger.debug("List command received...");
+
       BrokerCommand brokerCommand = new BrokerCommand();
       String inputCommand = SupportedCommands.LIST_COMMAND;
       String result = brokerCommand.executeCommand(inputCommand);
@@ -34,6 +42,9 @@ public class MainController {
 
   @GetMapping("/remove")
     public String remove(@RequestParam(value = "package") String packageName) {
+      
+      logger.debug("Remove command received...");
+
       BrokerCommand brokerCommand = new BrokerCommand();
       String inputCommand = SupportedCommands.REMOVE_COMMAND + " " + packageName;
       String result = brokerCommand.executeCommand(inputCommand);
@@ -43,6 +54,8 @@ public class MainController {
 
     @GetMapping("/end")
     public String end() {
+
+      logger.debug("End command received...");
 
       BrokerCommand brokerCommand = new BrokerCommand();
       String inputCommand = SupportedCommands.END_COMMAND;
@@ -54,7 +67,9 @@ public class MainController {
     //Request example: http://<server>:<port>/depend?package=ABC&dependency=A&dependency=B&dependency=C
     @GetMapping("/depend")
     public String depend(@RequestParam(value = "package") String packageName, @RequestParam(value = "dependency") List<String> dependencies) {
-    
+
+      logger.debug("Depend command received...");
+
       BrokerCommand brokerCommand = new BrokerCommand();
       String inputCommand = SupportedCommands.DEPEND_COMMAND + " " + packageName + " " + dependencies;
       String result = brokerCommand.executeCommand(inputCommand);
--- src/main/java/com/company/RestApiBootstrap.java
+++ src/main/java/com/company/RestApiBootstrap.java
@@ -1,12 +1,19 @@
 package com.company;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestApiBootstrap {
  
+	private static final Logger logger = LoggerFactory.getLogger(RestApiBootstrap.class);
+
 	public static void main(String[] args) {
+		logger.info("Initializing Package Manager...");
+
 		SpringApplication.run(RestApiBootstrap.class, args);
 	}
 }
